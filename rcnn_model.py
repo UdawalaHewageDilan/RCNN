f5236277c6b9971bab881c51ffb2f27e7808a1dc
Udawala Hewage Dilan <79456800+UdawalaHewageDilan@users.noreply.github.com>
2021-06-17 13:37:07 +0200

    Update rcnn_model.py

--- rcnn_model.py
+++ rcnn_model.py
@@ -15,7 +15,7 @@ class RCNN(nn.Module):
         self.pool1 = nn.MaxPool2d((2*2, 3))
         self.conv2 = nn.Conv2d(4*self.input_size, 16*self.input_size, 5, padding=(4, 4))
         self.pool2 = nn.MaxPool2d((2*2, 3))
-        #self.batch_norm = nn.BatchNorm2d(16*self.input_size, 1e-5)
+        self.batch_norm = nn.BatchNorm2d(16*self.input_size, 1e-5)
 
         self.gru = nn.GRU(self.input_size, 64, batch_first=True)
 
@@ -29,7 +29,7 @@ class RCNN(nn.Module):
         x = self.pool1(F.relu(self.conv1(x)))
         x = self.pool2(F.relu(self.conv2(x)))
 
-        #x = self.batch_norm(x)
+        x = self.batch_norm(x)
 
         x = x.view(32, 8, -1)
         x = x.permute(0, 2, 1)
